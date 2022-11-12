8c529be962e021fc4720dda1b1678d3b63bfe7cd
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2022-11-12 00:34:24 +0000

    initial commit

--- api/publish.js
+++ api/publish.js
@@ -1,1 +1,16 @@
+const tumblr = require("tumblr.js");
 
+export default function handler(request, response) {
+  const client = tumblr.createClient({
+    consumer_key: process.env.TMBLR_KEY,
+    consumer_secret: process.env.TMBLR_SECRET,
+  });
+
+  client.blogPosts("filmkit", function (err, resp) {
+    // console.log(resp.posts); // now we've got all kinds of posts
+
+    response.status(200).json({
+      body: resp.posts,
+    });
+  });
+}
