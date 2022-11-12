026c16b00f5d264618222a2620d9496475e721c1
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2022-11-12 00:40:56 +0000

    test

--- api/publish.js
+++ api/publish.js
@@ -6,7 +6,7 @@ export default function handler(request, response) {
     consumer_secret: process.env.TMBLR_SECRET,
   });
 
-  client.blogPosts("filmkit", function (err, resp) {
+  client.blogPosts(request.query.username, function (err, resp) {
     // console.log(resp.posts); // now we've got all kinds of posts
 
     response.status(200).json({
